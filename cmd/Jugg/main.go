d97134715f18c22fee01d216ee449e86a2b7e66f
ErikHallmark <ejhallmark@gmail.com>
2023-01-29 17:59:12 -0800

    begin refactoring to be more inline with standards

---
+++ cmd/Jugg/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/erikhallmark/jugg"
+	"github.com/spf13/pflag"
+)
+
+type cmdArgs struct {
+	port       string
+	outputFile string
+
+	baudRate int
+
+	silent bool
+	help   bool
+}
+
+func main() {
+	initCloseHandler()
+	var args cmdArgs
+	pflag.StringVarP(&args.port, "port", "p", "", "The com port you want to connect to")
+	pflag.StringVarP(&args.outputFile, "output", "p", "", "The name of a file you'd like Jugg to output too")
+
+	pflag.IntVarP(&args.baudRate, "baud", "b", 115200, "The baud rate of the device your connecting to (default is 115200)")
+
+	pflag.BoolVarP(&args.silent, "silent", "s", false, "Used to silence the console output")
+	pflag.BoolVarP(&args.help, "help", "h", false, "Show the help menu")
+
+	pflag.Parse()
+
+	var mode = pflag.Arg(0)
+
+	if args.help {
+		printHelpMenu()
+		os.Exit(0)
+	}
+
+	switch mode {
+	case "monitor":
+		break
+
+	case "list":
+		break
+
+	default:
+		if mode == "" {
+			fmt.Println("Please specify a mode")
+		} else {
+			fmt.Printf("%s is not a recognized mode", mode)
+		}
+	}
+
+}
+
+func ListDevices(args cmdArgs) {
+	var devices, err = jugg.ListDevices()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(list) == 0 {
+		fmt.Println("No serial devices found")
+	}
+
+	for i := 0; i < len(list); i++ {
+		var details = devices[i]
+		var name = details.Name
+		var product = details.Product
+		fmt.Printf("(%s) %s - %s", i, name, product)
+	}
+
+}
+
+func printHelpMenu() {
+	fmt.Println("Jugg Serial Port tool - V0.0.0 ")
+	fmt.Println("Usage: jugg [mode] [arguments] ")
+	fmt.Println("Modes:")
+	fmt.Println("  list		List the available serial port")
+	fmt.Println("  monitor	Monitors the activity on a serial port ")
+	fmt.Println("Arguments:")
+	fmt.Println("  --help	-h 	display this help menu")
+	fmt.Println("  --port	-p	set the serial port")
+	fmt.Println("  --baud	-b	set the baud rate")
+	fmt.Println("  --output	-o	output file")
+	fmt.Println("  --silent	-s	silent the output")
+}
+
+func initCloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		os.Exit(0)
+	}()
+}
---
+++ jugg.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tarm/serial"
+	"go.bug.st/serial/enumerator"
+)
+
+type PortDetails = enumerator.PortDetails
+
+func ListDevices() ([]*PortDetails, error) {
+	//TODO: Deal with potential errors
+	var list, err = enumerator.GetDetailedPortsList()
+	return list, err
+}
+
+func Monitor(port string, baud *int, file string, beSilent bool) {
+	var doOutput = file != ""
+	var f *os.File
+	if doOutput {
+		var err error
+		f, err = os.Create(file)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	c := &serial.Config{Name: port, Baud: *baud}
+	s, err := serial.OpenPort(c)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for true {
+		buf := make([]byte, 128)
+		n, err := s.Read(buf)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		var output = fmt.Sprintf("%s", buf[:n])
+
+		if !beSilent {
+			fmt.Print(output)
+		}
+
+		if doOutput {
+			_, err = f.WriteString(output)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
