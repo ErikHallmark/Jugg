b2bfd34d9e557effaed03643aef3c955972259dd
ErikHallmark <ejhallmark@gmail.com>
2023-02-01 21:21:06 -0800

    Improved monitor mode and help menus

--- cmd/jugg/main.go
+++ cmd/jugg/main.go
@@ -15,9 +15,7 @@ import (
 )
 
 type cmdArgs struct {
-	port       string
 	outputFile string
-	baudRate   int
 	silent     bool
 	verbose    bool
 	help       bool
@@ -26,10 +24,7 @@ type cmdArgs struct {
 func main() {
 	initCloseHandler()
 	var args cmdArgs
-	pflag.StringVarP(&args.port, "port", "p", "", "The com port you want to connect to")
 	pflag.StringVarP(&args.outputFile, "output", "o", "", "The name of a file you'd like Jugg to output too")
-
-	pflag.IntVarP(&args.baudRate, "baud", "b", 115200, "The baud rate of the device your connecting to (default is 115200)")
 
 	pflag.BoolVarP(&args.silent, "silent", "s", false, "Used to silence the console output")
 	pflag.BoolVarP(&args.help, "help", "h", false, "Show the help menu")
@@ -40,7 +35,7 @@ func main() {
 	var mode = pflag.Arg(0)
 
 	if args.help {
-		printHelpMenu()
+		printHelpMenu(pflag.Arg(0))
 		os.Exit(0)
 	}
 
@@ -55,25 +50,31 @@ func main() {
 		if mode == "" {
 			fmt.Println("Please specify a mode")
 		} else {
-			fmt.Printf("%s is not a recognized mode", mode)
+			fmt.Printf("%s is not a recognized mode. Use jugg --help for usage details", mode)
 		}
 	}
 
 }
 
 func monitorPort(args cmdArgs) {
-	incoming := make(chan jugg.PortData)
-	go jugg.MonitorPort(args.port, args.baudRate, incoming)
+	port := pflag.Arg(1)
+	baudRate, err := strconv.Atoi(pflag.Arg(2))
+	if err != nil {
+		e := fmt.Sprintf("%s is not a valid baud rate", pflag.Arg(2))
+		log.Fatal(e)
+	}
 
 	var f *os.File
 	if args.outputFile != "" {
 		var err error
 		f, err = os.Create(args.outputFile)
 		if err != nil {
-			log.Fatal()
+			log.Fatal(err)
 		}
 	}
 
+	incoming := make(chan jugg.PortData)
+	go jugg.MonitorPort(port, baudRate, incoming)
 	for {
 		update := <-incoming
 		output := string(update.Data)
@@ -145,18 +146,30 @@ func listDevices(args cmdArgs) {
 
 }
 
-func printHelpMenu() {
-	fmt.Println("Jugg Serial Port tool - V0.0.0 ")
-	fmt.Println("Usage: jugg [mode] [arguments] ")
-	fmt.Println("Modes:")
-	fmt.Println("  list		List the available serial port")
-	fmt.Println("  monitor	Monitors the activity on a serial port ")
-	fmt.Println("Arguments:")
-	fmt.Println("  --help	-h 	display this help menu")
-	fmt.Println("  --port	-p	set the serial port")
-	fmt.Println("  --baud	-b	set the baud rate")
-	fmt.Println("  --output	-o	output file")
-	fmt.Println("  --silent	-s	silent the output")
+func printHelpMenu(mode string) {
+	switch mode {
+	case "":
+		fmt.Println("Jugg Serial Port tool")
+		fmt.Println("Usage: jugg [mode] <arguments> [options]")
+		fmt.Println("Modes:")
+		fmt.Println("  list		List the available serial port")
+		fmt.Println("  monitor	Monitors the activity on a serial port ")
+		fmt.Println("Arguments:")
+		fmt.Println("  --help	-h 	display this help menu")
+		fmt.Println("  --output	-o	output file")
+		fmt.Println("  --silent	-s	silent the output")
+
+	case "monitor":
+		fmt.Println("Usage:	jugg monitor <port name> <baud rate> [options]")
+
+	case "list":
+		fmt.Println("Usage: jugg list [options]")
+
+	default:
+		fmt.Printf("%s is not recognized \r\n", mode)
+		printHelpMenu("")
+	}
+
 }
 
 func initCloseHandler() {
